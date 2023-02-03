6cd38ac05378adae1a3d9ffa8fa0a3017b7a8ae8
DMQuan03 <duongminhquan30052003@gmail.com>
2023-02-04 01:30:55 +0700

    Remove public/css folder

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const app = express();
+const port = 3001;
+
+// nap file logo trong img/public
+app.use(express.static(path.join(__dirname , 'public')))
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// Template engine
+app.engine('hbs' , engine({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs')
+app.set('views' , path.join(__dirname, 'resources/views'))
+
+
+app.get('/', (req , res) => res.render("home"));
+app.get('/new', (req , res) => res.render("new"));
+
+app.listen(port , () => console.log(`Example app Listening at http://localhost:${port}`));
